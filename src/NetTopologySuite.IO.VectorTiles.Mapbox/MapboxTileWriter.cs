0b16b553ac2c5f28d57c592cfb694b2e0436c1cb
kolyanch <kolyanch@outlook.com>
2022-11-29 20:52:15 +0400

    Removed unused using

--- src/NetTopologySuite.IO.VectorTiles.Mapbox/MapboxTileWriter.cs
+++ src/NetTopologySuite.IO.VectorTiles.Mapbox/MapboxTileWriter.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.IO;
 using NetTopologySuite.Features;
 using NetTopologySuite.Geometries;
-using NetTopologySuite.IO.VectorTiles.Tiles.WebMercator;
 
 namespace NetTopologySuite.IO.VectorTiles.Mapbox
 {
